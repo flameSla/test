3311f90855ecc923dc1b07ffe7554515d17c422b
flameSla <39356103+flameSla@users.noreply.github.com>
2024-01-17 21:47:19 +0700

    bps

---
+++ bps/bp_compiler_of_book.cmd
@@ -0,0 +1,5 @@
+py "D:\GitHub\bp_from_json\bps_compiler_of_book.py" -b="bp_compiler_out.out"
+
+@rem Y:\pypy310\pypy.cmd "D:\GitHub\bp_from_json\bps_into_folders.py"
+
+pause
---
+++ bps/bp_decompiler_of_book.cmd
@@ -0,0 +1,5 @@
+py "D:\GitHub\bp_from_json\bps_decompiler_of_book.py"
+
+@rem Y:\pypy310\pypy.cmd "D:\GitHub\bp_from_json\bps_into_folders.py"
+
+pause
